fc3736cc11eb69ca5e557e33eb8780f8e3a90d7d
drahmetacik <a.acik@icloud.com>
2024-12-10 13:54:17 +0000

    add sample user data and create README file

---
+++ data.py
@@ -0,0 +1,30 @@
+users = [
+    {
+        "id": "1",
+        "name": "Ahmet Doe",
+        "email": "ahmet@gmail.com",
+        "password": "123456",
+        "address": {
+            "street": "123 Main St",
+            "city": "Anytown",
+            "state": "CA",
+            "zip": "12345"
+        },
+        "phone": "555-1234",
+        "roles": ["admin", "user"]
+    },
+    {
+        "id": "2",
+        "name": "Mehmet Doe",
+        "email": "mehmet@gmail.com",
+        "password": "123456",
+        "address": {
+            "street": "456 Elm St",
+            "city": "Othertown",
+            "state": "TX",
+            "zip": "67890"
+        },
+        "phone": "555-5678",
+        "roles": ["user"]
+    }
+]
