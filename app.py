102852606e268fb65b0e0d78fae2f6cb5adb7009
drahmetacik <a.acik@icloud.com>
2025-10-18 18:35:06 +0100

    Refactor app.py by removing unused execute_and_print function and related test code to streamline the application

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+
 import graphene
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from mutations.create_user import CreateUser
 from mutations.update_user import UpdateUser
 from mutations.delete_user import DeleteUser
-from models.user import User  # Import User
-from data import users  # Import users list
+from models.user import User
+from data import users
 
 class MyMutations(graphene.ObjectType):
     create_user = CreateUser.Field()
@@ -22,170 +25,38 @@ class MyQuery(graphene.ObjectType):
 
 schema = graphene.Schema(query=MyQuery, mutation=MyMutations)
 
-def execute_and_print(label, query):
-    print(f"\n{label}")
-    result = schema.execute(query)
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    result = schema.execute(data.get("query"), variables=data.get("variables"))
+    response = {}
     if result.errors:
-        print("Errors:", result.errors)
-    else:
-        print(result.data)
+        response["errors"] = [str(e) for e in result.errors]
+    if result.data:
+        response["data"] = result.data
+    return jsonify(response)
 
-# Create a new user with a different id
-execute_and_print(
-    "Creating a new user with id 3:",
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    playground_html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>GraphQL Playground</title>
+      <link rel="stylesheet" href="https://unpkg.com/graphql-playground-react/build/static/css/index.css" />
+      <link rel="shortcut icon" href="https://graphql.org/img/favicon.png" />
+      <script src="https://unpkg.com/graphql-playground-react/build/static/js/middleware.js"></script>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script>window.addEventListener('load', function (event) { GraphQLPlayground.init(document.getElementById('root'), { endpoint: '/graphql' }) })</script>
+    </body>
+    </html>
     """
-mutation {
-  createUser(id: "3", name: "John Doe", email: "john@gmail.com", password: "abcdef", street: "789 Oak St", city: "Newtown", state: "NY", zip: "11223", phone: "555-7890", roles: ["user", "editor"]) {
-    user {
-      id
-      name
-      email
-      address {
-        street
-        city
-        state
-        zip
-      }
-      phone
-      roles
-    }
-  }
-}
-"""
-)
+    return playground_html
 
-# Update the newly created user
-execute_and_print(
-    "Updating the user with id 3:",
-    """
-mutation {
-  updateUser(id: "3", name: "John Updated", email: "john_updated@gmail.com", password: "newpassword", street: "789 Oak St", city: "Newtown", state: "NY", zip: "11223", phone: "555-7890", roles: ["admin"]) {
-    user {
-      id
-      name
-      email
-      address {
-        street
-        city
-        state
-        zip
-      }
-      phone
-      roles
-    }
-  }
-}
-"""
-)
-
-# Fetch all users before deleting the updated user
-execute_and_print(
-    "Fetching all users before deleting the user with id 3:",
-    """
-{
-  users {
-    id
-    name
-    email
-    address {
-      street
-      city
-      state
-      zip
-    }
-    phone
-    roles
-  }
-}
-"""
-)
-
-# Delete the newly created user
-execute_and_print(
-    "Deleting the user with id 3:",
-    """
-mutation {
-  deleteUser(id: "3") {
-    user {
-      id
-      name
-      email
-      address {
-        street
-        city
-        state
-        zip
-      }
-      phone
-      roles
-    }
-  }
-}
-"""
-)
-
-# Fetch all users after deleting the updated user
-execute_and_print(
-    "Fetching all users after deleting the user with id 3:",
-    """
-{
-  users {
-    id
-    name
-    email
-    address {
-      street
-      city
-      state
-      zip
-    }
-    phone
-    roles
-  }
-}
-"""
-)
-
-# Fetch a specific user by id
-execute_and_print(
-    "Fetching the user with id 1:",
-    """
-{
-  user(id: "1") {
-    id
-    name
-    email
-    address {
-      street
-      city
-      state
-      zip
-    }
-    phone
-    roles
-  }
-}
-"""
-)
-
-# Fetch a specific user by id
-execute_and_print(
-    "Fetching the user with id 1 using user query:",
-    """
-{
-  user(id: "1") {
-    id
-    name
-    email
-    address {
-      street
-      city
-      state
-      zip
-    }
-    phone
-    roles
-  }
-}
-"""
-)
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
