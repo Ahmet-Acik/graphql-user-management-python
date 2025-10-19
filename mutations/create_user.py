0b2895d7bbd99f8ee0100ddab49a215fe5230d8e
drahmetacik <a.acik@icloud.com>
2025-10-19 14:00:22 +0100

    Refactor CreateUser mutation to use SQLAlchemy for user creation and persist data to SQLite database

--- mutations/create_user.py
+++ mutations/create_user.py
@@ -1,6 +1,11 @@
 import graphene
 from models.user import User
-from data import users
+from models.user_model import UserModel
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///users.db')
+SessionLocal = sessionmaker(bind=engine)
 
 class CreateUser(graphene.Mutation):
     class Arguments:
@@ -27,9 +32,16 @@ class CreateUser(graphene.Mutation):
             create_user = CreateUser()
             create_user.user = None
             return create_user
+        db = SessionLocal()
+        # Convert roles list to comma-separated string
+        roles_str = ','.join(roles) if roles else ''
+        user_model = UserModel(id=id, name=name, email=email, password=password, phone=phone, roles=roles_str)
+        db.add(user_model)
+        db.commit()
+        db.refresh(user_model)
         address = {"street": street, "city": city, "state": state, "zip": zip}
-        user_data = {"id": id, "name": name, "email": email, "password": password, "address": address, "phone": phone, "roles": roles}
-        users.append(user_data)
+        user_data = {"id": str(user_model.id), "name": user_model.name, "email": user_model.email, "password": user_model.password, "address": address, "phone": user_model.phone, "roles": roles}
         create_user = CreateUser()
         create_user.user = User(**user_data)
+        db.close()
         return create_user
